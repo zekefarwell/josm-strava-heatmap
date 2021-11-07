6b8eecc5c624950ac7f6dd4a7c9f1c2635edc0d5
Zeke Farwell <ezekielf@gmail.com>
2021-11-07 00:01:27 -0400

    refactoring, resolving issues, and adding comments

--- content.js
+++ content.js
@@ -1,42 +1,30 @@
 
-insertModal();
-insertButton();
+insertModalHtml();
+insertButtonHtml();
 
-function insertModal()
+/**
+ * Insert HTML skeleton for modal dialog box.
+ * To be filled with content when the user clicks the button to open.
+ */
+function insertModalHtml()
 {
     document.body.insertAdjacentHTML('afterbegin', `
-        <div id="josm-modal" class="josm-modal">
-            <div id="josm-modal-dialog" class="josm-modal-dialog">
-                <div class="modal-header">
-                    <h4 id="josm-modal-message"></h5>
-                </div>
-                <div class="modal-body">
-                    <menu id="imagery-load-menu">
-                        <li>
-                            <a class="btn btn-xs btn-default" id="josm-click-to-load">Open imagery in JOSM</a>
-                        </li>
-                        <li>Or, copy the URL and paste into JOSM imagery preferences:
-                            <code>
-                                <button id="josm-click-to-copy" class="copy-button btn btn-xs" aria-label="Copy to clipboard" title="Copy to clipboard">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
-                                        <path d="M0 0h24v24H0V0z" fill="none" />
-                                        <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" />
-                                    </svg>
-                                </button>
-                                <pre id="josm-imagery-url"></pre>
-                            </code>
-                        </li>
-                    </menu>
-                </div>
+        <div id="jsh-modal" class="jsh-modal">
+            <div id="jsh-modal-dialog" class="jsh-modal-dialog">
+                <h4 id="jsh-modal-header" class="modal-header"></h4>
+                <div id="jsh-modal-body" class="modal-body"></div>
             </div>
         </div>
     `);
-    document.querySelector('#josm-modal').addEventListener("click", e => {
-        e.target.classList.remove('active')
+    document.querySelector('#jsh-modal').addEventListener("click", e => {
+        e.target.classList.remove('active');
     })
 }
 
-async function insertButton()
+/**
+ * Insert HTML for modal toggle button
+ */
+async function insertButtonHtml()
 {
     let ctrl_top_right = document.querySelector('.mapboxgl-ctrl-top-right');
     // Sometimes the mapbox controls aren't loaded right away and we need to wait a little bit
@@ -45,7 +33,7 @@ async function insertButton()
         ctrl_top_right = document.querySelector('.mapboxgl-ctrl-top-right');
     }
     let button = document.createElement('button');
-    button.className = 'josm-imagery';
+    button.className = 'jsh-modal-toggle';
     button.innerHTML = `
         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 48 48" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2">
         <defs>
@@ -58,53 +46,93 @@ async function insertButton()
         </defs>
         <path d="M46 32.8H32.8V46H46zm-30.8 0H2V46h13.2zm15.4 0H17.4V46h13.2zm0-15.4H17.4v13.2h13.2zm-15.4 0H2v13.2h13.2zm30.8 0H32.8v13.2H46zM15.2 2H2v13.2h13.2zM46 2H32.8v13.2H46zM30.6 2H17.4v13.2h13.2z" fill="url(#b)"/>
         </svg>
-    `
+    `;
     ctrl_top_right.prepend(button);
-    button.addEventListener("click", openJosmDialog);
+    button.addEventListener("click", openModalDialog);
 }
 
-async function openJosmDialog(e)
+/**
+ * Event listener function to open the modal dialog box and populate its content.
+ * If cookies are found, content will show the heatmap url and various actions.
+ * If not found, the content will show an error message instead.
+ */
+async function openModalDialog(e)
 {
-    let response,
-      message,
-      heatmap_url_manual_copy,
-      heatmap_url_click,
-      base_heatmap_url;
-
     let map_color = document.querySelector(".map-color.active").getAttribute("data-color");
     let map_type = document.querySelector(".map-type.active").getAttribute("data-type");
 
+    // Attempt to build the heatmap url from key pair, policy, and signature cookies
     try {
-        response = await browser.runtime.sendMessage({
+        let response = await browser.runtime.sendMessage({
             "name": "getHeatmapUrl",
             "map_color": map_color ?? "hot",
             "map_type": map_type ?? "all"
         });
         if (response.error) {
-            message = "Error: missing cookies"
-            heatmap_url_manual_copy = "One or more cookies not found - 'CloudFront-Key-Pair-Id', 'CloudFront-Policy', 'CloudFront-Signature'"
+            setModalHtmlError(
+                "Error: missing cookies", 
+                "One or more cookies not found - 'CloudFront-Key-Pair-Id', 'CloudFront-Policy', 'CloudFront-Signature'"
+            );
         } else {
-            base_heatmap_url = response.heatmap_url;
-            heatmap_url_manual_copy = "tms:" + base_heatmap_url;
-            heatmap_url_click =
-              "http://127.0.0.1:8111/imagery?title=Strava&type=tms&max_zoom=15&url=" +
-              base_heatmap_url;
-            message = "Open heatmap in editor"
+            setModalHtmlSuccess(response.heatmap_url, map_color, map_type);
         }
     } catch(err) {
-        console.log(err)
-        message = "Unknown error - check console"
-        heatmap_url_manual_copy = "couldn't build url"
+        console.log(err);
+        setModalHtmlError("Unknown error", "Couldn't build url.  Check console for errors.");
     }
 
-    document.querySelector('#josm-modal-message').textContent = message
-    document.querySelector('#josm-imagery-url').textContent = heatmap_url_manual_copy
-    document.querySelector("#josm-click-to-load").href = heatmap_url_click
-    document.querySelector('#josm-modal').classList.add('active');
-    document.querySelector("#josm-click-to-copy").addEventListener("click", copyUrlToClipboard);
+    // Open the modal now containing a success or failure message
+    document.querySelector('#jsh-modal').classList.add('active');
 }
 
-function copyUrlToClipboard() {
-    let heatmap_url_manual_copy = document.querySelector("#josm-imagery-url").innerHTML;
+/**
+ * Set the HTML content of the modal after successfully building the heatmap url
+ */
+function setModalHtmlSuccess(heatmap_url, map_color, map_type)
+{
+    let title = `Strava Heatmap (${map_color}/${map_type})`
+    let open_in_josm_url = `http://127.0.0.1:8111/imagery?title=${title}&type=tms&max_zoom=15&url=${heatmap_url}`;
+    let open_in_id_url = `https://www.openstreetmap.org/edit?editor=id&background=custom:${heatmap_url.replace(/&/g, '%26')}`;
+
+    document.querySelector('#jsh-modal-header').textContent = "Open heatmap in OSM editor";
+    document.querySelector('#jsh-modal-body').innerHTML = `
+        <p>
+            <a href="${open_in_josm_url}" target="_blank" rel="noopener noreferrer" class="btn btn-sm btn-default">
+                Open in JOSM
+            </a>
+            <a href="${open_in_id_url}" target="_blank" rel="noopener noreferrer" class="btn btn-sm btn-default">
+                Open in iD
+            </a>
+        </p>
+        <p>Or, copy the URL to manually add a custom imagery layer in your editor of choice: </p>
+        <code>
+            <button id="jsh-click-to-copy" class="copy-button btn btn-xs" aria-label="Copy to clipboard" title="Copy to clipboard">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+                    <path d="M0 0h24v24H0V0z" fill="none" />
+                    <path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z" />
+                </svg>
+            </button>
+            <pre id="jsh-imagery-url">${heatmap_url}</pre>
+        </code>
+    `;
+    document.querySelector("#jsh-click-to-copy").addEventListener("click", copyUrlToClipboard);
+}
+
+/**
+ * Set the HTML content of the modal with an error message
+ * after failure building the heatmap url
+ */
+function setModalHtmlError(header, body)
+{
+    document.querySelector('#jsh-modal-header').textContent = header;
+    document.querySelector('#jsh-modal-body').textContent = body;
+}
+
+/**
+ * Event listener function to copy the heatmap url on click
+ */
+function copyUrlToClipboard()
+{
+    let heatmap_url_manual_copy = document.querySelector("#jsh-imagery-url").textContent;
     navigator.clipboard.writeText(heatmap_url_manual_copy);
 }
